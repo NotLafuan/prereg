cbc55be3f43889296beeb335467fcd1f3f3e0581
Superstone1235 <naufalbakri122@gmail.com>
2022-12-25 14:41:55 +0800

    Add output on add_course

--- main.py
+++ main.py
@@ -3,37 +3,49 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import requests
+import lxml.html
 import os
 
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 
-def login(username: str, password: str) -> str:
-    with webdriver.Edge() as driver:
-        driver.get('https://prereg1.iium.edu.my/')
+class Prereg:
+    def login(self, username: str, password: str) -> None:
+        with webdriver.Edge() as driver:
+            driver.get('https://prereg1.iium.edu.my/')
 
-        frame = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.XPATH, '/html/frameset/frame[1]')))
-        driver.switch_to.frame(frame)
+            frame = WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.XPATH, '/html/frameset/frame[1]')))
+            driver.switch_to.frame(frame)
 
-        driver.find_element(By.NAME, 'mat').send_keys(username)
-        driver.find_element(By.NAME, 'pin').send_keys(password)
-        driver.find_element(By.NAME, 'Submit').click()
-        return driver.get_cookie('PHPSESSID')['value']
+            driver.find_element(By.NAME, 'mat').send_keys(username)
+            driver.find_element(By.NAME, 'pin').send_keys(password)
+            driver.find_element(By.NAME, 'Submit').click()
+            self.PHPSESSID = driver.get_cookie('PHPSESSID')['value']
 
-
-def add_course(course_code: str, section: str, PHPSESSID: str):
-    url = 'https://prereg1.iium.edu.my/addcourse.php?'
-    payload = {
-        'Course_code': course_code,
-        'Section': section,
-    }
-    headers = {'Cookie': f'PHPSESSID={PHPSESSID}'}
-    return requests.post(url, headers=headers, data=payload)
+    def add_course(self, course_code: str, section: str, print_output: bool = True):
+        url = 'https://prereg1.iium.edu.my/addcourse.php?'
+        payload = {
+            'Course_code': course_code,
+            'Section': section,
+        }
+        headers = {'Cookie': f'PHPSESSID={self.PHPSESSID}'}
+        response = requests.post(url, headers=headers, data=payload)
+        if print_output:
+            print(f'COURSE CODE: {course_code} | SECTION: {section}')
+            tree: lxml.html.HtmlElement = lxml.html.fromstring(response.text)
+            output: lxml.html.HtmlElement = tree.xpath('/html/body/p[2]')[0]
+            print(output.text_content())
+            print()
+        return response
 
 
 if __name__ == '__main__':
-    PHPSESSID = login(os.getenv('USERNAME'), os.getenv('PASSWORD'))
-    # add_course('MATH 2330', '3', PHPSESSID)
-    add_course('MCTA 2313', '1', PHPSESSID)
+    prereg = Prereg()
+    prereg.login(os.getenv('USER'), os.getenv('PASS'))
+    prereg.add_course('MATH 2330', '3')
+    prereg.add_course('MCTA 2102', '1')
+    prereg.add_course('MCTA 2313', '1')
+    prereg.add_course('MCTA 2314', '2')
+    prereg.add_course('MCTA 2315', '2')
